07a7685e5d0537983fab416da8635fa0ab830b59
eloranta <hannu.eloranta@gmail.com>
2025-11-29 10:23:02 +0200

    add db

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlTableModel>
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(":memory:");
+    if (!db.open())
+    {
+        QMessageBox::critical(this, "DB error", db.lastError().text());
+        return;
+    }
+
+    QSqlQuery q(db);
+    q.exec("CREATE TABLE items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, value INTEGER);");
+    q.exec("INSERT INTO items (name, value) VALUES ('Alpha', 1), ('Bravo', 2), ('Charlie', 3);");
+
+    auto *model = new QSqlTableModel(this, db);
+    model->setTable("items");
+    model->select();
+    ui->tableView->setModel(model);
+    ui->tableView->setSortingEnabled(true);
+    ui->tableView->resizeColumnsToContents();
 }
 
 MainWindow::~MainWindow()
