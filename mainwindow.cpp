ad4672668c66023def54f5fd00112021720d9cba
eloranta <hannu.eloranta@gmail.com>
2026-01-14 10:55:42 +0200

    loading dlls

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <QDebug>
+#include <hamlib/rig.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,6 +11,8 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->leftFrequency->setPrefix('A');
     ui->leftFrequency->setValue(14000000);
+
+    qDebug() << "hamlib version:" << hamlib_version;
 
     connect(ui->leftFrequency, &FrequencyLabel::valueChanged, this, [](int value, QChar prefix) {
         qDebug() << "Frequency value changed:" << prefix << value;
