4d7ba2c1793ffa41d6a9122cdfc4c1ca8801c22d
eloranta <hannu.eloranta@gmail.com>
2026-01-23 17:39:56 +0200

    refactor made rig static

--- tests/rig_test.cpp
+++ tests/rig_test.cpp
@@ -7,24 +7,26 @@ class RigTest : public QObject
     Q_OBJECT
 private slots:
     void setAndReadFrequency();
+private:
+    static Rig rig;
 };
 
 void RigTest::setAndReadFrequency()
 {
-    Rig rig(RIG_MODEL_TS590S, "COM7");
-
     if (!rig.open()) {
         qDebug() << "Hamlib rig_open failed:" << rig.lastError();
         return;
     }
-    bool ok = rig.setFrequency(7000001);
+    bool ok = rig.setFrequency(700010);
     QVERIFY(ok);
 
     int frequency = 0;
     ok = rig.readFrequency(frequency);
     QVERIFY(ok);
-    QCOMPARE(frequency, 7000001);
+    QCOMPARE(frequency, 700010);
 }
+
+Rig RigTest::rig(RIG_MODEL_TS590S, "COM7");
 
 QTEST_MAIN(RigTest)
 
